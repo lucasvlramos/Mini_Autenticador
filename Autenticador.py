139a0b1fc26b9b7cb2c673fdc6aac94c88fb9718
Lucas Venâncio <47925143+lucasvlramos@users.noreply.github.com>
2024-08-08 18:55:08 -0300

    Update Autenticador.py
    Algumas das mudanças:
    
    Importações: Adicionei as importações necessárias no início do arquivo.
    
    Configuração e Inicialização: Combinei a criação da aplicação Flask, configuração do banco de dados, e inicialização do SQLAlchemy em um bloco contínuo.
    
    Criação das Tabelas: Utilize o contexto da aplicação (app.app_context()) para garantir que as tabelas sejam criadas após a configuração do banco de dados e inicialização do SQLAlchemy.

--- Autenticador.py
+++ Autenticador.py
@@ -1,3 +1,20 @@
+from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__aut__)
+
+# Configuração do banco de dados SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cartoes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Configuração das credenciais básicas de autenticação HTTP
+app.config['HTTP_AUTH_USERNAME'] = 'username'
+app.config['HTTP_AUTH_PASSWORD'] = 'password'
+
+# Inicialização do SQLAlchemy
+db = SQLAlchemy(app)
+
 class Cartao(db.Model):
     numero_cartao = db.Column(db.String(16), primary_key=True)
     senha_hash = db.Column(db.String(128), nullable=False)
@@ -10,8 +27,13 @@ class Cartao(db.Model):
     def verificar_senha(self, senha):
         return check_password_hash(self.senha_hash, senha)
 
-
 def verificar_autenticacao():
     auth = request.authorization
     if not auth or auth.username != app.config['HTTP_AUTH_USERNAME'] or auth.password != app.config['HTTP_AUTH_PASSWORD']:
         abort(401)
+
+
+with app.app_context():
+    db.create_all()
+
+
